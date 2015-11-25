31e530106f0774308c571fd06a8e2b2275f5b07e
Phil Fluegel <pfluegelcx@gmail.com>
2015-11-25 17:51:49 -0600

    chk pt 19 assignment

--- models/address_book.rb
+++ models/address_book.rb
@@ -18,10 +18,15 @@ class AddressBook
 		@entries.insert(index, Entry.new(name, phone_number, email))
 	end
 	def remove_entry(name,phone_number,email)
-		@entries.delete_if do |entry|
-			if entry == name
+		delete_entry = nil
+
+		@entries.each do |entry|
+			if name == entry.name && phone_number == entry.phone_number && email == entry.email
+				delete_entry = entry
 			end
-			@entries
+
+			end
+			@entries.delete(delete_entry)
 		end
-	end
+	
 end
--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -38,17 +38,17 @@
    end
    describe "remove_entry" do
    	it "removes an entry from the address book" do
-   		book.AddressBook.new
-   		book.add_entry('phil fluegel', '010.555.5555' 'phil.king@lovelace.com')
+   		book = AddressBook.new
+   		book.add_entry('phil fluegel', '010.555.5555', 'phil.king@lovelace.com')
 
    		name = "Ada Lovelace"
    		phone_number = "010.012.1815"
    		email = "augusta.king@lovelace.com"
    		book.add_entry(name,phone_number,email)
 
-  		expect(book.entries.size).to eq(2)
+  		expect(book.entries.size).to eq 2
   		book.remove_entry(name,phone_number,email)
-  		expect(book.entries.size).to eg(1)
+  		expect(book.entries.size).to eq 1
   		expect(book.entries.first.name).to eq("phil fluegel")
 
    	end
