fae7c8ac28c609a8609aadf59300a8572b6f2484
Phil Fluegel <pfluegelcx@gmail.com>
2015-11-25 13:16:07 -0600

    End of Checkpoint 19

--- models/entry.rb
+++ models/entry.rb
@@ -7,7 +7,7 @@ class Entry
      	@email = email
    	end
    	def to_s
-   		"Name: #{@name}\nPhone Number:#{@phone_number}\nEmail: #{@email}"
+   		"Name: #{@name}\nPhone Number: #{@phone_number}\nEmail: #{@email}"
    	end
  
 end
--- spec/entry_spec.rb
+++ spec/entry_spec.rb
@@ -5,19 +5,19 @@ RSpec.describe Entry do
     it "should respond to name" do
       entry = Entry.new('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
                
-      entry = Entry.new
+
       expect(entry).to respond_to(:name)
     end
 
       it "should respond to phone_number" do
       entry = Entry.new('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')   
-      entry = Entry.new
+
       expect(entry).to respond_to(:phone_number)
     end
 
     it "should respond to email" do
       entry = Entry.new('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com') 
-      entry = Entry.new
+
       expect(entry).to respond_to(:email)
     end
   end
